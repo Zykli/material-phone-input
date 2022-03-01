00be148c968d7ae09c134c4f23cebcaa274b8dae
Zykli <zyklizen@gmail.com>
2022-03-01 23:06:32 +0300

    alone mask worked

--- src/phone-input/CountrySelector.tsx
+++ src/phone-input/CountrySelector.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ctrs from './countries';
+import { SelectUnstyled } from '@mui/base';
+// import MenuItem from '@mui/material/MenuItem';
+// import Select from '@mui/material/Select';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { List, AutoSizer } from 'react-virtualized';
+import Button, { buttonClasses } from '@mui/material/Button';
+import { Box } from '@mui/material';
+
 
 const countries = ctrs.map(e => {
     let flag = `https://flagcdn.com/${e.iso.toLowerCase()}.svg`;
@@ -7,28 +17,110 @@ const countries = ctrs.map(e => {
         flag = `https://flagcdn.com/nl.svg`;
     }
     return {
-        ...e, flag 
+        ...e,
+        flag 
     }
 });
 
-interface Props {
-    // selected: typeof countries[number]['iso'];
+(window as any).countries = countries
 
+interface Props {
+    selected: typeof countries[number]['iso'];
+    onChange: (mask: string | string[]) => void;
 }
 
 export const CountrySelector: React.FC<Props> = ({
-
+    selected,
+    onChange
 }) => {
+
+    const [ image, setImage ] = React.useState<typeof countries[number]['flag'] | null>(null);
+
+    React.useEffect(() => {
+        const item = countries.find((e) => e.iso === selected);
+        if(item) setImage(item.flag)
+    }, [selected]);
+    
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div>
-            {
-                countries.map((e, idx) => {
-                    return <>
-                    {idx}
-                        <img src={e.flag} style={{width: 30, height: 20}} />
-                    </>
-                })
-            }
+            <Button
+                size="small"
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+                sx={{
+                    minWidth: 54,
+                    [`& .${buttonClasses.endIcon}`]: {
+                        ml: 0
+                   }
+                }}
+                
+            >
+                {image && <img src={image} style={{width: 30, height: 20}} />}
+            </Button>
+            <Menu
+                id="demo-positioned-menu"
+                aria-labelledby="demo-positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+            >
+                <List 
+                    height={200}
+                    rowCount={countries.length}
+                    rowHeight={32}
+                    width={300}
+                    rowRenderer={({ index, key, style }) => {
+                        const data = countries[index];
+                        return (
+                            <MenuItem
+                                key={key}
+                                style={style}
+                                onClick={() => {
+                                    setImage(data.flag);
+                                    if(Array.isArray(data.mask)) {
+                                        onChange(data.mask.map(el => `${data.code} ${el}`));
+                                    } else {
+                                        onChange(`${data.code} ${data.mask}`);
+                                    }
+                                    handleClose();
+                                }}
+                                sx={{
+                                    display: 'flex',
+                                    alignItems: 'flex-start'
+                                }}
+                                title={data.name}
+                            >
+                                <Box sx={{mr: 1}}>
+                                    <img src={data.flag} style={{width: 30, height: 20}} />
+                                </Box>
+                                <span style={{
+                                    overflow: 'hidden',
+                                    textOverflow: 'ellipsis'
+                                }}>
+                                    {data.name}
+                                </span>
+                            </MenuItem>
+                        )
+                    }}
+                />
+            </Menu>
         </div>
     )
 }
--- src/phone-input/PhoneInput.tsx
+++ src/phone-input/PhoneInput.tsx
@@ -3,24 +3,27 @@ import { IMaskInput } from "react-imask";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import { CountrySelector } from "./CountrySelector";
+import { array } from "yargs";
 
 
 interface CustomProps {
     onChange: (event: { target: { name: string; value: string } }) => void;
     name: string;
-  }
+    mask: string;
+}
   
-  const TextMaskCustom = React.forwardRef<HTMLElement, CustomProps>(
+const TextMaskCustom = React.forwardRef<HTMLElement, CustomProps>(
     function TextMaskCustom(props, ref) {
       const { onChange, mask, ...other } = props as any;
       console.log("other", other);
+      console.log("mask", mask);
       const baseMask = "#(000) 0000000";
       return (
         <IMaskInput
           {...other}
-          mask={mask || baseMask}
+          mask={mask}
           definitions={{
-            "#": /[1-9]/
+            "#": /[1-90]/
           }}
           inputRef={ref}
           onAccept={(value: any) =>
@@ -35,22 +38,41 @@ interface CustomProps {
   
 
 interface State {
-    textmask: string;
-    numberformat: string;
+    country: React.ComponentProps<typeof CountrySelector>['selected'];
+    mask: string;
+    value: string;
 }
   
 export default function FormattedInputs() {
     const [values, setValues] = React.useState<State>({
-      textmask: "2(100) 000-0000",
-      numberformat: "1320"
+        value: '',
+        mask: "",
+        country: 'RU'
     });
   
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setValues({
-        ...values,
-        [event.target.name]: event.target.value
-      });
+        let value = event.target.value;
+        if(!value.length) {
+            value = values.mask.replace(/[#\(\)-]/g, '');
+        }
+        setValues({
+            ...values,
+            [event.target.name]: value
+        });
     };
+
+    const setMask: React.ComponentProps<typeof CountrySelector>['onChange'] = (mask) => {
+        console.log(mask);
+        if(Array.isArray(mask)) {
+
+        } else {
+            setValues({
+              ...values,
+              mask: mask,
+              value: mask.replace(/#/g, '0')
+            });
+        }
+    }
   
     return (
       <Box
@@ -60,15 +82,18 @@ export default function FormattedInputs() {
           }
         }}
       >
-        <CountrySelector />
         <TextField
           label="react-number-format"
-          value={values.textmask}
+          value={values.value}
           onChange={handleChange}
-          name="textmask"
+          name="value"
           id="formatted-numberformat-input"
           InputProps={{
-            inputComponent: TextMaskCustom as any
+            inputComponent: TextMaskCustom as any,
+            startAdornment: <CountrySelector selected={values.country} onChange={setMask} />
+          }}
+          inputProps={{
+            mask: values.mask
           }}
         />
       </Box>
